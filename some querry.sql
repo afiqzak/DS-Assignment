e6e5df9238d281dfaac3d2e9623b922193bbdd30
afiqzak <148320546+afiqzak@users.noreply.github.com>
2024-05-10 14:48:39 +0800

    Add files via upload

---
+++ some querry.sql
@@ -0,0 +1,10 @@
+insert into customer
+values 	(10001, 'ali', 01012321, 'adhwiu@gmail.com', '12345', '2004-12-02', 'jhjerbcknwkc'),
+		(10002, 'abu', 01023917, 'ucbwiubc@gmail.com', '11123', '2004-12-02', 'kjjdbbciw'),
+        (10003, 'amad', 01233892, 'hdhbw9ubcd@gmail.com', '218t38', '2004-12-02', 'coecoiebd');
+        
+insert into admin
+value (01, 'abu', 010213231, 'uxwiuxbiwbu@gmail.com', '1221eg9');
+
+
+        
