7c165eda790ee65f9c1362c5ffa6dc99828a2f82
afiqzak <148320546+afiqzak@users.noreply.github.com>
2024-05-10 14:57:11 +0800

    Update some querry.sql

--- some querry.sql
+++ some querry.sql
@@ -1,10 +1,18 @@
 insert into customer
 values 	(10001, 'ali', 01012321, 'adhwiu@gmail.com', '12345', '2004-12-02', 'jhjerbcknwkc'),
-		(10002, 'abu', 01023917, 'ucbwiubc@gmail.com', '11123', '2004-12-02', 'kjjdbbciw'),
+	(10002, 'abu', 01023917, 'ucbwiubc@gmail.com', '11123', '2004-12-02', 'kjjdbbciw'),
         (10003, 'amad', 01233892, 'hdhbw9ubcd@gmail.com', '218t38', '2004-12-02', 'coecoiebd');
         
 insert into admin
-value (01, 'abu', 010213231, 'uxwiuxbiwbu@gmail.com', '1221eg9');
+values (01, 'abu', 010213231, 'uxwiuxbiwbu@gmail.com', '1221eg9');
 
+insert into account
+values 	('1234 1233 1234', 10001, 100000, 'Platinum Patronus'),
+	('1245 1233 1312', 10002, 100000, 'Silver Snitch'),
+        ('7342 2313 1232', 10003, 100000, 'Golden Galleon')
 
+insert into transaction (ID_Transaction, AccountNum, Amount, Type, Description)
+values 	(1001, '1234 1233 1234', 10, 'entertainment', 'iedciuwb'),
+	(1002, '1245 1233 1312', 102, 'food', 'chrbhieubc'),
+        (1003, '7342 2313 1232', 101, 'essential', 'dceiw')
         
