885267e1a4f76189702a10eca64d816f419f99dd
wenhu <wenhu@LAPTOP-CQCK0N8Q>
2024-05-15 11:56:13 +0800

    Marauder's Map

--- egringotts/src/marauderMap/ConnectionManager.java
+++ egringotts/src/marauderMap/ConnectionManager.java
@@ -8,7 +8,7 @@ public class ConnectionManager {
 
     private static final String url = "jdbc:mysql://localhost:3306/egringotts?zeroDateTimeBehavior=CONVERT_TO_NULL";
     private static final String user = "root";
-    private static final String password = "";
+    private static final String password = "WenHui0411";
     private static final String drive_class = "com.mysql.cj.jdbc.Driver";
 
     static {
